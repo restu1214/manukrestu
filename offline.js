1322d9b6764999bad9456f39e62df97e3064ec0b
restu1214 <161668117+restu1214@users.noreply.github.com>
2024-03-07 08:20:48 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1709774221,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backroundflappybird-sheet0.png",
+		"images/2393204_segathi_flappybirdnightbackground-sheet0.png",
+		"images/flyinggamecharacterremovebgpreview-sheet0.png",
+		"images/flyinggamecharacterremovebgpreview-sheet1.png",
+		"images/tiangremovebgpreview-sheet0.png",
+		"images/tiangremovebgpreview2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
